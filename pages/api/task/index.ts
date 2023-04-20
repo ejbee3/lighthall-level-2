a321c1765c6da2ffb449ef22f3e684dbfc4d8514
jo bars <joe.baranski49@gmail.com>
2023-04-20 01:55:30 -0400

    this is very broken sorry

---
+++ pages/api/task/index.ts
@@ -0,0 +1,15 @@
+import prisma from "../../../lib/prisma";
+
+export default async function handle(req, res) {
+    const {title, description, dueDate } = req.body
+
+    const result = await prisma.task.create({
+        data: {
+            title: title,
+            description: description,
+            isNew: true,
+            dueDate: dueDate
+        }
+    })
+    res.json(result)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import React from "react"
-import { GetStaticProps } from "next"
+import React, { useState } from "react"
+import { GetServerSideProps } from "next"
 import Layout from "../components/Layout"
 import Task, { TaskProps } from "../components/Task"
 import prisma from '../lib/prisma';
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const list = await prisma.task.findMany({
     where: { isNew: true },
     include: {
@@ -14,16 +14,34 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   })
   return {
-    props: { list },
-    revalidate: 10,
+    props: { list }
   }
 }
+
 
 type Props = {
   list: TaskProps[]
 }
 
 const TaskList: React.FC<Props> = (props) => {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [dueDate, setDueDate] = useState('')
+
+  const submitData = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    try {
+      const body = { title, description, dueDate }
+      await fetch('/api/task', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <Layout>
       <div className="page">
@@ -34,6 +52,27 @@ const TaskList: React.FC<Props> = (props) => {
               <Task task={task} />
             </div>
           ))}
+          <div>
+            <form onSubmit={submitData}>
+              <input
+                autoFocus
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Title"
+                type="text"
+                value={title}
+              />
+              <textarea
+                cols={50}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Description"
+                rows={8}
+                value={description}
+              />
+              <input type="date" name="dueDate"
+                min="2023-04-01" max="2025-12-31" value={dueDate} onChange={(e) => setDueDate(e.target.value)} required pattern="\d{4}-\d{2}-\d{2}"></input>
+              <input disabled={!description || !title} type="submit" value="Create" />
+            </form>
+          </div>
         </main>
       </div>
       <style jsx>{`
@@ -48,6 +87,38 @@ const TaskList: React.FC<Props> = (props) => {
 
         .task + .task {
           margin-top: 2rem;
+        }
+
+        .page {
+          background: var(--geist-background);
+          padding: 3rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        input[type='text'],
+        textarea {
+          width: 100%;
+          padding: 0.5rem;
+          margin: 0.5rem 0;
+          border-radius: 0.25rem;
+          border: 0.125rem solid rgba(0, 0, 0, 0.2);
+        }
+
+        input[type='submit'] {
+          background: #ececec;
+          border: 0;
+          padding: 1rem 2rem;
+        }
+
+        input[type='submit']:hover {
+          border: 0.25rem solid black;
+        }
+
+        input[type='submit']:focus {
+          background: #5A5A5A;
+          color: #f5f5f5;
         }
       `}</style>
     </Layout>
