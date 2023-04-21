c1b078d0ebc227ef54e963c43176b85a9e2dd490
jo bars <joe.baranski49@gmail.com>
2023-04-21 17:46:15 -0400

    ready to deploy

--- components/Task.tsx
+++ components/Task.tsx
@@ -17,9 +17,11 @@ const Task: React.FC<{ task: TaskProps }> = ({ task }) => {
 
   const status = task.isNew ? "New" : task.isInProgress ? "In progress" : task.isCompleted ? "Completed" : "";
 
+  const formatDueDate = (date: string) => `${date.slice(5, 7)}-${date.slice(-2)}-${date.slice(0, 4)}`;
+
   return (
     <div onClick={() => Router.push("/p/update/[id]", `/p/update/${task.id}`)}>
-      <small>{status}</small>
+      <small>{status} - <span>due: {formatDueDate(task.dueDate)}</span></small>
       <h2 className={task.isCompleted ? "completed" : "none"}>{task.title}</h2>
       <h2>By {task.userName}</h2>
       <ReactMarkdown children={task.description} />
--- pages/tasks.tsx
+++ pages/tasks.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { GetServerSideProps, GetStaticProps } from "next"
+import React, { useState } from "react"
+import { GetServerSideProps } from "next"
 import Layout from "../components/Layout"
 import Task, { TaskProps } from "../components/Task"
 import prisma from '../lib/prisma';
@@ -25,15 +25,52 @@ type Props = {
 
 const TaskList: React.FC<Props> = (props) => {
 
+    const [tasks, setTasks] = useState(props.list);
+    const [sortMode, setSortMode] = useState('dueDate');
 
+    switch (sortMode) {
+        case 'dueDate':
+            tasks.sort((a, b) => {
+                // sort by due date
+                if (a.dueDate < b.dueDate) {
+                    return -1;
+                }
+            })
+            break;
+        case 'title':
+            tasks.sort((a, b) => {
+                // sort alphabetically
+                if (a.title < b.title) {
+                    return -1;
+                }
+            })
+            break;
+        case 'status':
+            tasks.sort((a, b) => {
+                // sort by status
+                const statusA = a.isNew ? "New" : a.isInProgress ? "In progress" : a.isCompleted ? "Completed" : "";
+                const statusB = b.isNew ? "New" : b.isInProgress ? "In progress" : b.isCompleted ? "Completed" : "";
+
+                if (statusA > statusB) {
+                    return -1;
+                }
+            })
+            break;
+    }
 
     return (
         <Layout>
             <div className="page">
                 <h1>Tasks</h1>
                 <main>
+                    <label htmlFor="sort">Sort by: </label>
+                    <select onChange={(e) => setSortMode(e.target.value)} name="sort">
+                        <option value="dueDate">Due Date</option>
+                        <option value="title">Title</option>
+                        <option value="status">Status</option>
+                    </select>
                     <button onClick={() => Router.push('/create')}>new task</button>
-                    {props.list.map((task) => (
+                    {tasks.map((task) => (
                         <div key={task.id} className="task">
                             <Task task={task} />
                         </div>
@@ -41,27 +78,27 @@ const TaskList: React.FC<Props> = (props) => {
                 </main>
             </div>
             <style jsx>{`
-        .task {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
+          .task {
+            background: white;
+            transition: box-shadow 0.1s ease-in;
+          }
+  
+          .task:hover {
+            box-shadow: 1px 1px 3px #aaa;
+          }
 
-        .task:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
+            .task + .task {
+                margin-top: 2rem;
+              }
 
-        .task + .task {
-          margin-top: 2rem;
-        }
-
-        .page {
-          background: var(--geist-background);
-          padding: 3rem;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-      `}</style>
+              .page {
+                background: var(--geist-background);
+                padding: 3rem;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
+            `}</style>
         </Layout>
     )
 }
